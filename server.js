e83734f28b8e05ff544cb97688c46ba664c598d0
frobbins <robbinsfatim@gmail.com>
2020-09-08 17:30:13 -0600

    Included Express data parsing, listening to PORT

--- server.js
+++ server.js
@@ -3,6 +3,21 @@ var express = require("express") ;
 var path = require("path");
 var fs = require("fs");
 
+
 //Sets up the Express App
 var app = express();
-var PORT = 3000;
+var PORT = 7500;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "Develop/public")));
+
+//Notes (DATA)
+var noteInfo = [];
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+  
+
